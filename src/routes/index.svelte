054700d1342f1d153d74173cac4938db11cde729
ashwinky <ashwin.yka@gmail.com>
2022-02-25 18:29:32 +0000

    svelte

---
+++ src/routes/index.svelte
@@ -0,0 +1,16 @@
+<svelte:head>
+  <title>Kheti Badi App</title>
+</svelte:head>
+
+<section class="section">
+  <div class="container">
+    <h1 class="title">Kheti Badi App</h1>
+    <p class="subtitle">
+      We're using <a href="https://bulma.io/"><b>Bulma</b></a> +
+      <a href="https://kit.svelte.dev/"><b>Svelte</b></a> now!
+    </p>
+    <figure class="image is-128x128">
+      <img alt="cute boi" src="/modi.jpg" />
+    </figure>
+  </div>
+</section>
